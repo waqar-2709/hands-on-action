c28c6b37ee11a0d6eb773935b73414e60cd6f9b0
waqar-2709 <123267380+waqar-2709@users.noreply.github.com>
2023-03-27 19:02:31 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,2 +1,18 @@
-# Terraform Test Code 
-# Branchtest10 Test
+    terraform {
+    backend "remote" {
+      # The name of your Terraform Cloud organization.
+       organization = "waqar-ahmed-2709"
+
+#         # The name of the Terraform Cloud workspace to store Terraform state files in.
+      workspaces {
+        name = "example-workspace"
+       }
+    }
+  }
+
+#     # An example resource that does nothing.
+    resource "null_resource" "example" {
+       triggers = {
+         value = "A example resource that does nothing!"
+       }
+     }
