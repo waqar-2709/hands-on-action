84b476d52e244dee3cf571f2314d8963308d1b25
Waqar Ahmed <blackops0285@gmail.com>
2023-03-27 20:21:19 -0400

    ec2 instance

--- main.tf
+++ main.tf
@@ -11,8 +11,13 @@
   }
 
 #     # An example resource that does nothing.
-    resource "null_resource" "example" {
-       triggers = {
-         value = "A example resource that does nothing!"
-       }
-     }
+  provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAR27VBNN7OQJAHLVR"
+  secret_key = "M1pNuaP1L5Dr/lLEG8n7p1unzelL/FuUHv1SI2PH"
+
+}
+resource "aws_instance" "example" {
+  ami           = "ami-2757f631"
+  instance_type = "t2.micro"
+}
